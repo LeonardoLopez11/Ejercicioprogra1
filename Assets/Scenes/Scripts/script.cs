69143e6805563b84fc4ebd87bfc1f923566c216b
LeonardoLopez11 <85042599+LeonardoLopez11@users.noreply.github.com>
2023-08-29 21:24:21 -0500

    Proyecto y creacion de metodo

---
+++ Assets/Scenes/Scripts/script.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 5;
+        int b = 10;
+
+
+        public class operacion
+        {
+            public int suma(int a, int b){
+                return a + b;
+            
+            }
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
